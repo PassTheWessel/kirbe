438fb8d2ace3f5a5d8509168e7f5a00711e24841
David Ralph <ohlookitsderpy@protonmail.com>
2019-02-18 11:52:14 +0000

    general code improvements part 2

--- model/KirbeResponse.js
+++ model/KirbeResponse.js
@@ -1,50 +1,47 @@
 module.exports = class KirbeResponse {
-  constructor( res ) {
+  constructor(res) {
     this.coreRes = res;
-		this.headers = {};
+    this.headers = {};
 
-		this.statusCode    = 200;
-		this.statusMessage = null;
+    this.statusCode    = 200;
+    this.statusMessage = null;
 
-		this.data = Buffer.alloc( 0 );
+    this.data = Buffer.alloc(0);
   }
 
-  body( body ) {
-    if ( typeof body === 'object' && !Buffer.isBuffer( body ) ) {
-			if ( !this.headers[ 'content-type' ] ) this.headers[ 'content-type' ] = 'application/json';
+  body(body) {
+    if (typeof body === 'object' && !Buffer.isBuffer(body)) {
+	if (!this.headers['content-type']) this.headers['content-type'] = 'application/json';
+        this.data = JSON.stringify(body);
+    } else this.data = body;
 
-			this.data = JSON.stringify( body );
-		} else this.data = body;
-
-		return this;
+    return this;
   }
 
-  header( a, b ) {
-    if ( typeof a === 'object' ) Object.keys( a ).forEach( ( v ) => this.headers[ v.toLowerCase() ] = a[ v ] );
-		else this.headers[ a.toLowerCase() ] = b;
-
-		return this;
+  header(a, b) {
+    if (typeof a === 'object') Object.keys(a).forEach((v) => this.headers[v.toLowerCase()] = a[v]);
+    else this.headers[a.toLowerCase()] = b;
+	  
+    return this;
   }
 
-  status( code, message ) {
-    this.statusCode = code;
-		this.statusMessage = message;
-
-		return this;
+  status(code, message) {
+    this.statusCode    = code;
+    this.statusMessage = message;
+	  
+    return this;
   }
 
-  writeHead( status, headers ) {
-    if ( status ) this.status( status );
-		if ( headers ) this.header( headers );
+  writeHead(status, headers) {
+    if (status)  this.status(status);
+    if (headers) this.header(headers);
   }
 
-  end( data ) {
-    if ( data ) this.body( data );
-
-		if (this.statusMessage) this.coreRes.writeHead( this.statusCode, this.statusMessage, this.headers );
-		else this.coreRes.writeHead( this.statusCode, this.headers );
-		this.coreRes.end( this.data );
-
-		return this;
+  end(data) {
+    if (data) this.body(data);
+    if (this.statusMessage) this.coreRes.writeHead(this.statusCode, this.statusMessage, this.headers);
+    else this.coreRes.writeHead(this.statusCode, this.headers);
+    this.coreRes.end(this.data);
+    return this;
   }
 };
